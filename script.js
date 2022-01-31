01f95b5e118f7e86fc568d3772bfcc7176d32503
Rstar284 <supermanrehan284@gmail.com>
2022-01-31 13:08:51 +0400

    Separate Code, Make `Copy Link` button work

---
+++ script.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sbtn = document.getElementById('sbtn')
+    const cbtn = document.getElementById('cbtn')
+    const code = document.getElementById('code')
+    const code2 = document.getElementById('code2')
+    function copy() {navigator.clipboard.writeText(window.location.href).then(function () {alert('Copied to clipboard!')})}
+    cbtn.addEventListener('click', copy)
+    code.disabled = false
+    cbtn.style.display = 'none'
+    sbtn.style.display = 'inline-block'
+    code.focus()
+    code2.style.display = 'none'
+    function escapeHTML(str) {
+        return str
+            .replace(/&/g, '&amp;')
+            .replace(/>/g, '&gt;')
+            .replace(/</g, '&lt;')
+            .replace(/"/g, '&quot;')
+    }
+    const extensionMap = {
+        rb: 'ruby',
+        py: 'python',
+        pl: 'perl',
+        php: 'php',
+        scala: 'scala',
+        go: 'go',
+        xml: 'xml',
+        html: 'xml',
+        htm: 'xml',
+        css: 'css',
+        js: 'javascript',
+        vbs: 'vbscript',
+        lua: 'lua',
+        pas: 'delphi',
+        java: 'java',
+        cpp: 'cpp',
+        cc: 'cpp',
+        m: 'objectivec',
+        vala: 'vala',
+        sql: 'sql',
+        sm: 'smalltalk',
+        lisp: 'lisp',
+        ini: 'ini',
+        diff: 'diff',
+        bash: 'bash',
+        sh: 'bash',
+        tex: 'tex',
+        erl: 'erlang',
+        hs: 'haskell',
+        md: 'markdown',
+        txt: '',
+        coffee: 'coffee',
+        swift: 'swift',
+    }
+    const lookupTypeByExtension = function (ext) {
+        return extensionMap[ext] || ext
+    }
+    const b64 = location.hash.substr(1)
+    if (b64) {
+        const decoded = atob(b64)
+        code.disabled = true
+        code.style.display = 'none'
+        code2.style.display = 'block'
+        code2.innerHTML = escapeHTML(decoded)
+        sbtn.style.display = 'none'
+        cbtn.style.display = 'inline-block'
+        const params = new URLSearchParams(window.location.search)
+        if (params.has('lang')) {
+            if (params.get('lang') === null || params.get('lang') === '') return
+            const lang = params.get('lang').toLowerCase()
+            code2.innerHTML = hljs.highlight(decoded, {
+                language: lookupTypeByExtension(lang),
+                ignoreIllegals: true,
+            }).value
+            document.title = lang ? 'Rpaste - Base64 - ' + lang : 'TXT'
+            return
+        } else {
+            hljs.highlightElement(code2)
+            return
+        }
+    }
+    function save() {
+        const base64 = btoa(code.value)
+        location.hash = base64
+        location.reload()
+    }
+    sbtn.addEventListener('click', save)
+    
+})
